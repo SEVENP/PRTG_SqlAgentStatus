4433ea201fcea75209cee1fd4c56a596a7ead021
Leonard de Ruijter <L.de.Ruijter@sevenp.nl>
2021-06-30 13:05:13 +0200

    Add sensor script and lookup file

---
+++ Get-SqlAgentStatus.ps1
@@ -0,0 +1,28 @@
+[CmdletBinding()]
+Param (
+	[Parameter(Mandatory)]
+	[String]$SqlInstance
+)
+try
+{
+	Import-Module DbaTools
+	$Jobs = Get-DbaAgentJob -SqlInstance $SqlInstance -ExcludeDisabledJobs -EnableException
+	$result = $jobs | ForEach-Object {
+		@{
+			channel     = $_.Name;
+			value       = [Int32]$_.LastRunOutcome;  # Zie 
+			valuelookup = 'SqlAgentJob.ExecutionResult';
+		}
+	}
+	$prtg = @{ prtg = @{ result = $result; }; }
+}
+catch
+{
+	$prtg = @{
+		prtg = @{
+			error   = 1;
+			message = $_.Exception.Message;
+		};
+	}
+}
+$prtg | ConvertTo-Json -Depth 3 | Write-Output
